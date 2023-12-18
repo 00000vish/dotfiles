4e6ddfbb2428624790bc800ca461ac952a096aeb
vishwa <11762008+00000vish@users.noreply.github.com>
2023-12-17 20:38:00 -0600

    gruvbox commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,21 @@
 set fish_greeting
 
-# TokyoNight Color Palette
-set -l foreground c0caf5
-set -l selection 283457
-set -l comment 565f89
-set -l red f7768e
-set -l orange ff9e64
-set -l yellow e0af68
-set -l green 9ece6a
-set -l purple 9d7cd8
-set -l cyan 7dcfff
-set -l pink bb9af7
+# Dracula Color Palette
+set -l foreground ebdbb2
+set -l selection 504945
+set -l comment 8f3f71
+set -l red fb4934
+set -l orange fe8019
+set -l yellow fabd2f
+set -l green b8bb26
+set -l purple d3869b
+set -l cyan 8ec07c
+set -l blue 83a598
 
 # Syntax Highlighting Colors
 set -g fish_color_normal $foreground
 set -g fish_color_command $cyan
-set -g fish_color_keyword $pink
+set -g fish_color_keyword $blue
 set -g fish_color_quote $yellow
 set -g fish_color_redirection $foreground
 set -g fish_color_end $orange
@@ -25,7 +25,7 @@ set -g fish_color_comment $comment
 set -g fish_color_selection --background=$selection
 set -g fish_color_search_match --background=$selection
 set -g fish_color_operator $green
-set -g fish_color_escape $pink
+set -g fish_color_escape $blue
 set -g fish_color_autosuggestion $comment
 
 # Completion Pager Colors
@@ -33,7 +33,6 @@ set -g fish_pager_color_progress $comment
 set -g fish_pager_color_prefix $cyan
 set -g fish_pager_color_completion $foreground
 set -g fish_pager_color_description $comment
-set -g fish_pager_color_selected_background --background=$selection
 
 alias vim "nvim"
 alias code "code --disable-gpu-sandbox"
