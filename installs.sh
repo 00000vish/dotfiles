84e5d5d9afaededc0f8b1b3bd1d7a881dbe7fd25
vishwa <11762008+00000vish@users.noreply.github.com>
2024-06-03 18:10:30 -0500

    add install script

---
+++ installs.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+echo " "
+echo "[::]Updating System[::]"
+echo " "
+sudo pacman -Syu
+echo " "
+
+echo " "
+echo "[::]Installing System[::]"
+echo " "
+sudo pacman -S \
+     firefox \
+     mpv \
+     gimp \
+     networkmanager-openvpn \
+     pipewire \
+     bluez \
+     bluez-utils \ 
+     fish \
+     timeshift \
+     hyprpaper \ 
+     alacritty \
+     rofi \
+     waybar \
+     starship \
+     git \ 
+     zip \ 
+     unzip \
+     tmux \
+     neovim \
+echo " "
+
+echo " "
+echo "[::]Installing...[::]"
+echo " "
+sudo pacman -S qemu-full \ 
+               libvirt \
+               dnsmasq \
+               swtpm \
+               virt-manager 
+echo " "
+
+sudo usermod -a -G libvirt $USER
+sudo systemctl enable libvirtd.service
+sudo systemctl start libvirtd.service
+
+echo " "
+echo "[::] Install Complete [::]"
