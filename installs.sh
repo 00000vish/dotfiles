ecb713551e6cafdd6c5c38398a2a891ad34446c9
vishwa <11762008+00000vish@users.noreply.github.com>
2024-06-04 21:27:21 -0500

    fix install.sh

--- installs.sh
+++ installs.sh
@@ -9,7 +9,7 @@ echo " "
 echo " "
 echo "[::]Installing System[::]"
 echo " "
-sudo pacman -S firefox mpv gimp networkmanager-openvpn pipewire bluez bluez-utils fish timeshift hyprpaper alacritty rofi waybar starship git zip unzip tmux neovim plasma-meta
+sudo pacman -S firefox mpv gimp networkmanager-openvpn pipewire bluez bluez-utils fish timeshift hyprpaper alacritty rofi waybar starship git zip unzip tmux neovim plasma-meta qt6ct
 echo " "
 
 echo " "
@@ -21,6 +21,12 @@ echo " "
 sudo usermod -a -G libvirt $USER
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
+
+echo " "
+echo "[::]Uninstalling Junk[::]"
+echo " "
+sudo pacman -Runs wofi kitty 
+echo " "
 
 echo " "
 echo "[::] Install Complete [::]"
